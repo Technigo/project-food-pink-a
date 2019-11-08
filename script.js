e4aed469f5fb2a7a8661ebf2afda64d327c3e616
AleksandraMuller <aleksandra.e.mueller@gmail.com>
2019-11-08 10:48:53 +0100

    filter on price range

--- script.js
+++ script.js
@@ -6,14 +6,45 @@ const cuisineId = 168;
 const url = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&cuisines=${cuisineId}`;
 
 const restaurants = []
+const container = document.querySelector('.container')
+
 
 fetch(url, { headers: { "user-key": apiKey } })
   .then(res => res.json())
   .then(json => {
     console.log(json);
 
-    json.restaurants.forEach(el => {
-        document.querySelector('.container').innerHTML += `<div class="boxes"><h2>${el.restaurant.name}</h2><p>${el.restaurant.average_cost_for_two}</p><p>${el.restaurant.user_rating.aggregate_rating}</p><img src="${el.restaurant.featured_image}"><p>${el.restaurant.location.address}</p></div>`
-    })
+    // let restaurant = json.restaurants
+
+   
+
+
+//     json.restaurants.forEach(el => {
+        
+
+//         const price = el.restaurant.average_cost_for_two
+//         const showLowPrice = () => {
+//             if (price <= 40) {
+//                 document.querySelector('.container').innerHTML += `<div class="boxes"><h2>${el.restaurant.name}</h2><p>${el.restaurant.average_cost_for_two}</p><p>${el.restaurant.user_rating.aggregate_rating}</p><img src="${el.restaurant.featured_image}"><p>${el.restaurant.location.address}</p></div>`
+//             }
+//         }
+//         const showMediumPrice = () => {
+//             if (price >= 41 && price <= 59) {
+//                 document.querySelector('.container').innerHTML += `<div class="boxes"><h2>${el.restaurant.name}</h2><p>${el.restaurant.average_cost_for_two}</p><p>${el.restaurant.user_rating.aggregate_rating}</p><img src="${el.restaurant.featured_image}"><p>${el.restaurant.location.address}</p></div>`
+//             }
+//         }
+//         const showHighPrice = () => {
+//             if (price >= 60 ) {
+//                 document.querySelector('.container').innerHTML += `<div class="boxes"><h2>${el.restaurant.name}</h2><p>${el.restaurant.average_cost_for_two}</p><p>${el.restaurant.user_rating.aggregate_rating}</p><img src="${el.restaurant.featured_image}"><p>${el.restaurant.location.address}</p></div>`
+//             }
+//         }
+// document.getElementById("lowPrice").addEventListener("click", showLowPrice)
+// document.getElementById("mediumPrice").addEventListener("click", showMediumPrice)
+// document.getElementById("highPrice").addEventListener("click", showHighPrice)
+
+        // console.log(el.restaurant.price_range, el.restaurant.average_cost_for_two)
+    // })
+    
+
     
   });
